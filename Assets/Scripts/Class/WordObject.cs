2790e9fd0047f821ddf49bfc0a54367c13b79247
jasson31 <jasson31@naver.com>
2019-09-06 20:40:53 +0900

    기본적인 단어 생성 및 뇌 공간 생성

---
+++ Assets/Scripts/Class/WordObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordObject : MonoBehaviour
+{
+
+    public string wordText = null; //Text of this word object
+    public int wordGrade, wordWeight;
+
+    public TextMesh textMesh; //Text field(text mesh) of this word object
+
+
+    public void Initiate(string _wordText, Vector2 pos)
+    {
+        wordText = _wordText;
+        textMesh.text = wordText;
+        transform.position = pos;
+    }
+
+    private void LateUpdate()
+    {
+        if (transform.eulerAngles.z > 30 && transform.eulerAngles.z < 180) transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 30);
+        else if (transform.eulerAngles.z < 330 && transform.eulerAngles.z > 180) transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, -30);
+    }
+}
---
+++ Assets/Scripts/WordSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordSpawner : MonoBehaviour
+{
+    public WordObject wordObject;
+
+    //Positions of initial word object, temporal value
+    float minX = -3, maxX = 3, initialY = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) Instantiate(wordObject).Initiate("쌀국수", new Vector2(Random.Range(minX, maxX), initialY));
+    }
+}
