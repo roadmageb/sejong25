2c0d40c70962edc470a91aa23b9983dce496222b
손재민 <jasson31@naver.com>
2019-09-16 15:54:21 +0900

    플레이어 닉네임 및 아이디 아주 간단하게 구현

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    string _userName = "홍길동"; //Name of this player
+    public string UserName { get => _userName; set => _userName = value; }
+
+
+    int _userID = 12345;
+    public int UserID { get => _userID; }
+
+}
